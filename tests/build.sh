7cf552318d9e9b7903c5a21f35b042afbf77e256
Prem chavhan <pchavhan@logiquad.com>
2023-08-04 16:14:01 +0530

    updated build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cp -r docker/single-process/* ./
-
+rm -f docker-compose.yml
+mv docker-compose-new.yml docker-compose.yml
 docker buildx build . --output type=docker,name=elestio4test/huginn:latest | docker load
